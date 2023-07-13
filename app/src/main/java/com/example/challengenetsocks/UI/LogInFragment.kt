a370eeb3c327833aa04856bf4ebedfa6c3257397
unknown <yonatanezeq@gmail.com>
2023-07-12 21:32:41 -0300

    commit inicial

---
+++ app/src/main/java/com/example/challengenetsocks/UI/LogInFragment.kt
@@ -0,0 +1,12 @@
+package com.example.challengenetsocks.UI
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.challengenetsocks.R
+
+class LogInFragment : Fragment(R.layout.fragment_log_in) {
+
+}
---
+++ app/src/main/java/com/example/challengenetsocks/UI/SingInFragment.kt
@@ -0,0 +1,8 @@
+package com.example.challengenetsocks.UI
+
+import androidx.fragment.app.Fragment
+import com.example.challengenetsocks.R
+
+class SingInFragment : Fragment(R.layout.fragment_sing_in) {
+
+}
